7c3d059ab395a56e3fb35c70995c2071fa017a13
Pascal <netpascal0123@aol.com>
2019-12-11 22:24:01 +0100

    Allow ir data to be sent as variable by home assistant

--- ir.h
+++ ir.h
@@ -5,13 +5,21 @@
 extern IRsend irsend;
 IRsend irsend = IRsend(D5);
 
-class IRsendWrapper : public Component {
+class IRsendWrapper : public Component, public CustomAPIDevice, public Sensor {
  public:
   void setup() override {
         
         irsend.begin();
+
+        /* https://esphome.io/custom/custom_component.html?highlight=custom_component#native-api-custom-component */
+        register_service(
+            &IRsendWrapper::send_nec, 
+            "send_nec",
+            {"data", "nbits", "repeat"}
+        );
   }
-  void loop() override {
-    // This will be called by App.loop()
+  void send_nec(int data, int nbits, int repeat) {
+    ESP_LOGD("custom", "Launch function irsend.sendNEC(%#010x, %d, %d)", data, nbits, repeat);
+    irsend.sendNEC(data, nbits, repeat);
   }
 };
